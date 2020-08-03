d281c394c8660b2beaa02609d3bfe885d2509f57
zlw <zlw20010124@163.com>
2020-08-03 23:00:19 +0800

    邮件，登陆与注册

--- src/main/java/com/cqupt/demo/Controller/ApiController.java
+++ src/main/java/com/cqupt/demo/Controller/ApiController.java
@@ -7,12 +7,13 @@ import com.cqupt.demo.Bean.User;
 import com.cqupt.demo.Service.AdminService;
 import com.cqupt.demo.Service.UserService;
 import com.cqupt.demo.utils.UserUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -28,6 +29,9 @@ public class ApiController {
     @Resource
     private AdminService adminService;
 
+    @Resource
+    JavaMailSenderImpl mailSender;
+
     /**
      * 普通用户注册
      *
@@ -35,7 +39,7 @@ public class ApiController {
      * @return
      */
     @PostMapping("/user")
-    public Map<String, Object> addUser(@RequestBody User user) {
+    public Map<String, Object> addUser(@RequestBody User user) throws MessagingException {
         Map<String, Object> modelMap = new HashMap<String, Object>();
         if (!UserUtil.isNull(user)) {
             User temp = userService.queryUser(user.getUserName());
@@ -44,9 +48,19 @@ public class ApiController {
                 modelMap.put("Msg", "用户名已注册");
                 return modelMap;
             }
-            userService.addUser(user);
-            modelMap.put("success", true);
-            modelMap.put("Msg", "注册成功");
+            if(userService.addUser(user)==1) {
+                SimpleMailMessage message = new SimpleMailMessage();
+                message.setSubject("welcome to video system !!!!");
+                String text = "恭喜你注册成功\n你的账号为:" + user.getUserName() + "\n" + "密码为:" + user.getPassword();
+                message.setText(text);
+                message.setTo(user.getEmail());
+                message.setFrom(mailSender.getUsername());
+                mailSender.send(message);
+                modelMap.put("success", true);
+                modelMap.put("Msg", "注册成功");
+            }else{
+                modelMap.put("Msg", "用户注册失败");
+            }
         } else {
             modelMap.put("success", false);
             modelMap.put("Msg", "用户信息不能为空,请重新输入");
@@ -69,9 +83,10 @@ public class ApiController {
                 modelMap.put("Msg","用户名已存在");
                 return modelMap;
             }
-            adminService.addAdmin(admin);
-            modelMap.put("success", true);
-            modelMap.put("Msg","注册成功");
+                adminService.addAdmin(admin);
+                modelMap.put("success", true);
+                modelMap.put("Msg", "注册成功");
+
         }else{
             modelMap.put("success", false);
             modelMap.put("Msg","注册失败");
@@ -81,11 +96,26 @@ public class ApiController {
 
     }
 
+    /**
+     * 普通用户登陆
+     * @param userName
+     * @param password
+     * @param request
+     * @return
+     */
     @PostMapping("/login")
     public JSONObject login(@RequestParam String userName, @RequestParam String password, HttpServletRequest request){
         HttpSession session = request.getSession(true);
         return userService.login(userName, password, session);
     }
+
+    /**
+     * 管理员用户登陆
+     * @param adminName
+     * @param password
+     * @param request
+     * @return
+     */
     @PostMapping("/adlogin")
     public JSONObject adlogin(@RequestParam String adminName, @RequestParam String password, HttpServletRequest request){
         HttpSession session = request.getSession(true);
